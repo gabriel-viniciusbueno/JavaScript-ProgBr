acc7f454bc67a199348d6e96baa4973f5fb9f8e3
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2022-11-23 09:46:19 -0300

    Introdução aos operadores condicionais

---
+++ exercicios/ex12/script.js
@@ -0,0 +1,21 @@
+// Estudo dos operadores condicionais
+
+
+/* Exemplo com console.log
+
+var idade = 18;
+
+ if (idade >= 18) {
+     console.log("Pode beber!")
+ } else {
+     console.log("Não tem idade ainda")
+
+*/
+
+var idade = parseInt(prompt("Quantos anos você tem?"));
+
+if (idade >= 18){
+    alert("Pode beber!");
+} else{
+    alert("Não tem idade ainda.");
+}
