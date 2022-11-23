fed7d82fdeee947baeabf6b3da1dfc4c81991396
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2022-11-23 10:10:24 -0300

    Alguns exercícios de condicionais

--- exercicios/ex12/script.js
+++ exercicios/ex12/script.js
@@ -14,8 +14,18 @@ var idade = 18;
 
 var idade = parseInt(prompt("Quantos anos você tem?"));
 
-if (idade >= 18){
+
+// if (idade >= 18 && idade <= 70){
+//     alert("Pode beber!");
+// } else{
+//    alert("Você não pode beber aqui");
+// }
+
+
+if(idade < 18 || idade > 70){
+    alert("Não pode beber")
+}else{
     alert("Pode beber!");
-} else{
-    alert("Não tem idade ainda.");
 }
+
+
