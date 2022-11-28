0d993caaa73c8d977c8a4775f488bed6e35173e8
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2022-11-28 13:47:41 -0300

    Update script.js

--- exercicios/ex14/script.js
+++ exercicios/ex14/script.js
@@ -21,8 +21,32 @@ if(media >= 8){
     conceito = "Regular";
 
 } else{
-    conceito = "Péssimo, estude mais";
+    conceito = "Ruim";
     
 }
+
+conceito = "Mais ou Menos";
+
 console.log(media);
 console.log(conceito);
+
+
+
+switch(conceito){
+    
+    case conceito = "Ótimo!":
+        console.log("Você foi muito bem!")
+        break
+    case conceito = "Regular":
+        console.log("Você foi mediano")
+        break
+    case conceito = "Ruim":
+        console.log("Bem ruim ai, precisa estudar mais")
+        break
+    default:
+        console.log("Houve algum erro")    
+        break
+}
+
+
+//Se por algum acaso nós mudarmos a variável conceito para "mais ou menos", switch deixará de funcionar pois não encontrará esse valor em seus cases. Logo, vai disparar a mensagem de erro (default)
