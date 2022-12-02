013c8eed4d7870671db24ef716d5ceca735698ce
Gabriel Bueno <106624338+gabriel-viniciusbueno@users.noreply.github.com>
2022-12-02 09:08:29 -0300

    Praticando FOR e SWITCH

---
+++ exercicios/ex21_revisao/for.js
@@ -0,0 +1,28 @@
+//Estudo de FOR;
+
+var alunos = ["gabriel", "maria", "isabela", "carlinhos", "josias"];
+var notaA = [10, 9.5, 5.5, 7.0, 9.0];
+var notaB = [10, 3.5, 7.0, 5.5, 1.5];
+
+var media = function(n1,n2){
+    return (n1 + n2) /2;
+}
+
+function passou(media){
+    if(media > 6){
+        return "Aprovado!";
+    } else{
+        return "Reprovado";
+    }
+}
+
+for(var index in alunos){
+
+    var nota1 = notaA[index];
+    var nota2 = notaB[index];
+    var aluno = alunos[index];
+
+    var m = media(nota1, nota2);
+
+    console.log(aluno + " - " + nota1 + " - " + nota2 + " - " + m + " - " + passou(m));
+}
